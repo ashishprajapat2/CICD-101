0b93bb35d3b6a4b7d049643a4c1616971d52c018
Ashish Prajapat <ashishprajapat2@users.noreply.github.com>
2025-08-17 16:34:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from inside a container!\n');
+  res.end('Hello from a CI/CD Pipeline!\n');
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+
 });
